a5f0ac639f8d9545ed1badccf9abcac1fe7af8d4
Muzzu Pasha <muzahirraza509@gmail.com>
2025-01-29 16:55:40 +0530

    changes

--- index.js
+++ index.js
@@ -4,6 +4,35 @@ const express = require('express')
 const app = express()
 const port = 4000
 
+const githubdata = {
+    "data": [{
+      "type": "articles",
+      "id": "1",
+      "attributes": {
+        "title": "JSON:API paints my bikeshed!",
+        "body": "The shortest article. Ever.",
+        "created": "2015-05-22T14:56:29.000Z",
+        "updated": "2015-05-22T14:56:28.000Z"
+      },
+      "relationships": {
+        "author": {
+          "data": {"id": "42", "type": "people"}
+        }
+      }
+    }],
+    "included": [
+      {
+        "type": "people",
+        "id": "42",
+        "attributes": {
+          "name": "John",
+          "age": 80,
+          "gender": "male"
+        }
+      }
+    ]
+  }
+
 app.get('/', (req, res) => {
   res.send('Hello World!')
 })
@@ -14,6 +43,10 @@ res.send("twitter")
 
 app.get('/insta', (req, res)=>{
 res.send("hello insta")
+})
+
+app.get('/github', (req, res)=>{
+    res.json(githubdata)
 })
 
 app.listen(process.env.PORT, () => {
